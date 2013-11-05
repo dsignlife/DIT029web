ff6156bc4ca05708f3ba7fc2a51c6c298e9d6f3f
dsignlife <dsign-life@hotmail.com>
2013-11-05 11:27:51 +0100

    -ADDED CSS for the search button -ADDED INDEX.JS FOR THE CSS BOTTON -CHANGED CONTENT OF INDEX.HTML -ADDED SOME LOCAL HISTOTICAL PRICES FOR TESTING

---
+++ WebContent/index.js
@@ -0,0 +1,54 @@
+// each indicator needs to have a unique name in the indicators object
+// the value is an array of the parameters used to define the indicaotr
+$(function() {
+  $('#ticker').focus();
+  refreshChart('#ticker');
+  $('#ticker').bind('keypress', function(e) {
+    if(e.which==13){
+      refreshChart('#ticker');
+    }
+  });
+  $('#ticker').click(function(){
+    $('#ticker').select();
+  });
+  $('#go_button').click(function(){
+    refreshChart('#ticker');
+  });
+  $('.history').on('click', 'li', function(){
+    $('#ticker').val($(this).text());
+    refreshChart('#ticker');
+  });
+});
+var refreshChart = function(selector){
+  var ticker = $(selector).val().toUpperCase();
+  var options = {
+    title: ticker+' weekly'
+    , adjust: 1
+    , indicators : [
+        ['EMA', 'c', 26]
+      , ['SMA', 'c', 200]
+      , ['MACD', 12, 26, 9]
+    ]
+  };
+  $.get("weekly."+ticker+".txt",function(data) {
+    $('.history').html(addHistory(ticker));
+    var chart = new Candlestick("myChart",data, options);
+    console.log(chart);
+  }).fail(function() { 
+    $.get("daily."+ticker+".txt",function(data) {
+      $('.history').html(addHistory(ticker));
+      options.title = ticker+' daily';
+      var chart = new Candlestick("myChart",data, options);
+      console.log(chart);
+    }).fail(function() { alert('Ticker not found.'); });
+  });
+  $(selector).select();
+}
+var addHistory = function(ticker){
+ if (typeof addHistory.tickers == 'undefined' ) {
+   addHistory.tickers = [ticker];
+ } else if(addHistory.tickers.indexOf(ticker)==-1) {
+   addHistory.tickers.push(ticker);
+ }
+ return '<ul><li>'+addHistory.tickers.join('</li><li>')+'</li></ul>';
+}
